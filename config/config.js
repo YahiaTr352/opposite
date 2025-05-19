99899d81caaf401aac5922f3df17eff445646c44
YahiaTr352 <yahiatr188@gmail.com>
2025-05-20 00:53:14 +0300

    last last last commit

--- config/config.js
+++ config/config.js
@@ -1,13 +1,35 @@
+// const mongoose = require("mongoose");
+
+// const ConnectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://localhost:/PaymentGateWay");
+//     console.log("✅ Connected to MongoDB");
+
+//   } catch (error) {
+//     console.error("❌ Failed to connect to MongoDB:", error);
+//   }
+// };
+
+// module.exports = ConnectDB;
+
+
 const mongoose = require("mongoose");
+const uri = "mongodb+srv://user1:yahiamo99@cluster0.bxkm2rk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const ConnectDB = async() => {
 
-const ConnectDB = async () => {
-  try {
-    await mongoose.connect("mongodb://localhost:/PaymentGateWay");
-    console.log("✅ Connected to MongoDB");
+    try{
+    mongoose.connect(uri, {
+        ssl: true, // تأكد من أن الاتصال باستخدام SSL
+    })
 
-  } catch (error) {
-    console.error("❌ Failed to connect to MongoDB:", error);
-  }
-};
+    console.log("connected to mongodb");
+
+    }catch(error){
+        console.log("failed to connect to mongoDB");
+        console.log("..............................................");
+        console.log(error);
+    }
+}
+
 
 module.exports = ConnectDB;
