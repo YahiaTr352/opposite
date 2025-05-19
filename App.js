d9a7933d1eb53ef48a14342476e0dcc75b3223cd
YahiaTr352 <yahiatr188@gmail.com>
2025-05-20 02:48:52 +0300

    last last last last last last commit

--- App.js
+++ App.js
@@ -9,9 +9,26 @@ const ensureTTLIndex = require("./utils/ensureTTLIndex");
   const app = express();
     const port = process.env.Port || 5000;
 
+    const allowedOrigins = [
+      'http://localhost:3001',
+      'https://paymentgateway-0bks.onrender.com' // عدّلها حسب اسم موقعك ع Render
+    ];
+
+    app.use(cors({
+      origin: function(origin, callback) {
+        // السماح بالطلبات بدون origin (مثل Postman أو نفس السيرفر)
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true
+    }));
+
     // ✅ إعدادات عامة
-    const BASE_API_URL2 = "http://localhost:3000"; 
-    app.use(cors());
+    // const BASE_API_URL2 = "https://paymentgateway-0bks.onrender.com"; 
+    // app.use(cors({ origin: BASE_API_URL2, credentials: true }));
     app.use(express.json());
 
     // ✅ اتصالات بقاعدة البيانات
@@ -25,5 +42,5 @@ const ensureTTLIndex = require("./utils/ensureTTLIndex");
 
     // ✅ تشغيل السيرفر
     app.listen(port, () => {
-      console.log(` Server is running on port ${port}`);
+      console.log(`✅ Server is running on port ${port}`);
     });
